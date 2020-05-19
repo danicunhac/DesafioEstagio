9192bae5ea1c8f87b5214f226d724e918cf4206b
Daniel <danielcc.jp@gmail.com>
2020-05-19 18:14:45 -0300

    Preparando ambiente de trabalho

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/** Evidencia os estados existentes como não completados (false) */
+const initialState = {
+    gerInit: false,
+    gerEnd: false,
+    regInit: false,
+    regEnd: false,
+    revInit: false,
+    revEnd: false
 }
+
+const App = () => {
+  const [phases, setPhases] = useState(initialState);
+
+  /** Método para iniciar a transição de estados do documento */
+  const initFlow = (id) => {
+    const currentState = phases[id];
+    if(currentState === undefined) return null;
+
+    const completedPhase = Object.values(phases).filter((value) => value)
+  };
+
+  return (
+      <div className="container">
+          <h1>Simulação de Transição de Estados de um Documento</h1>
+      </div>
+  )
+};
 
 export default App;
